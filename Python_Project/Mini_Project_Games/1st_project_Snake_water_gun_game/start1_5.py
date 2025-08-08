a2d181941d753541eb689429e798ef37cb7c7f55
693WeeB <69xabdurrahman@gmail.com>
2025-08-09 02:24:33 +0600

    .

---
+++ Python_Project/Mini_Project_Games/1st_project_Snake_water_gun_game/start1_5.py
@@ -0,0 +1,22 @@
+import random
+
+computer = random.choice([-1, 0, 1])
+user = input("Enter your choice : ")
+
+list = {"s": 1, "w": -1, "g": 0}
+you = list[user]
+reverselist = {0: "gun", -1: "water", 1: "snake"}
+
+print("Computer chose ", reverselist[computer])
+
+if (computer == you):
+    print("You both chose the same\nit's a drow")
+else:
+    if (computer == 1 and you == -1):
+        print("You Lose")
+    elif (computer == -1 and you == 0):
+        print("You Lose")
+    elif (computer == 0 and you == 1):
+        print("You Lose")
+    else:
+        print("You Win")
