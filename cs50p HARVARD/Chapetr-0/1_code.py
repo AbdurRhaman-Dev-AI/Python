012cd47857d62ebc7ed59b81fbb2db6bdd3c0f41
693WeeB <69xabdurrahman@gmail.com>
2025-08-25 18:12:19 +0600

    Code from HARVARD cource on python

---
+++ cs50p HARVARD/Chapetr-0/1_code.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: \n")
+
+print("Hello", name)
+print("Hello", name, sep="  ") # # sep is the separator it uses between the values
+print("Hello", name, sep="--")  # # and yap we can use any thing to separate the values
+# print("Hello 'hi' marin")
+# print('Hello "hi" marin')
+# print("Hello \"hi\" marin")
+# print("Hello \'hi\' marin")
---
+++ cs50p HARVARD/Chapetr-0/2_calculation.py
@@ -0,0 +1,23 @@
+# x = int(input("Enter a number: "))
+# y = int(input("Enter a number: "))
+# z = x + y
+
+# print(z)
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+
+
+A = float(input("Enter a number: "))
+B = float(input("Enter a number: "))
+C = A + B
+D = A - B
+E = A * B
+F = round(A / B, 2)
+G = A / B
+# print("Addition:", C)
+# print("Subtraction:", D)
+# print("Multiplication:", E)
+print("Division:", F)
+print("Division:", G)
