4f19cde95c1795cbbe7772ca304b980cc52a9131
693WeeB <69xabdurrahman@gmail.com>
2025-09-01 18:56:36 +0600

    CS50P - Lecture 0

---
+++ CS50P - Harvard/CS50P - Lecture 0/definde.py
@@ -0,0 +1,18 @@
+# def hello(to="Everyone"):
+#     print("Hello", to)
+# hello()
+# name = input("What's your name? ")
+# hello(name)
+
+
+
+
+def main():
+    name = input("What's your name? ")
+    hello(name)
+
+
+def hello(to="Everyone"):
+    print("Hello", to)
+
+main()
--- cs50p HARVARD/Chapetr-0/definde.py
+++
@@ -1,5 +0,0 @@
-def hello(to="Everyone"):
-    print("Hello", to)
-hello()
-name = input("What's your name? ")
-hello(name)
