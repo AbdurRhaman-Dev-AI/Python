61782a38534532033e9b3f58186f3e4d17889561
693WeeB <69xabdurrahman@gmail.com>
2025-09-04 03:04:54 +0600

    loops

--- CS50P - Harvard/CS50P - Lecture 2/For_loops.py
+++ CS50P - Harvard/CS50P - Lecture 2/For_loops.py
@@ -1,2 +1,7 @@
-for i in range(12):
+# for i in range(12):
+#     print("Hello")
+
+
+n = int(input("Number: "))
+for i in range(n):
     print("Hello")
--- CS50P - Harvard/CS50P - Lecture 2/While_loop.py
+++ CS50P - Harvard/CS50P - Lecture 2/While_loop.py
@@ -1,4 +1,11 @@
-i = 0
-while i < 5:
-    print("Hello")
-    i = i + 1
+# i = 0
+# while i < 5:
+#     print("Hello")
+#     i = i + 1
+
+
+
+x = int(input("Number: "))
+while x > 0:
+    print("gay")
+    x = x - 1
