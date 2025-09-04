f451bdb676124c05a283820e0159ee63ceb39789
693WeeB <69xabdurrahman@gmail.com>
2025-09-04 14:42:37 +0600

    list & dickt

---
+++ CS50P - Harvard/CS50P - Lecture 2/dickt.py
@@ -0,0 +1,8 @@
+student = {
+    "name": "John",
+    "age": 21,
+    "major": "Computer Science"
+}
+for i in student:
+    # print(i)
+    print(i, student[i], sep=": ")
---
+++ CS50P - Harvard/CS50P - Lecture 2/list.py
@@ -0,0 +1,11 @@
+student = [
+    {"name": "Abdullah", "age": 20, "major": "Fullstack Development"},
+    {"name": "Marin", "age": 21, "major": "AI-ML networking"},
+    {"name": "Safin", "age": 22, "major": "Graphic Design"},
+    {"name": "Saif", "age": 22, "major": "None"},
+]
+
+for i in student:
+    # print(i["name"])
+    # print(i["name"], i["age"], sep=": ")
+    print(i["name"], i["age"], i["major"], sep=": ")
