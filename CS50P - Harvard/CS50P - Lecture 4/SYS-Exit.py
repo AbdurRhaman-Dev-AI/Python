2fc14b7c12c2bb7c1817077c8b30db15058422a9
693WeeB <69xabdurrahman@gmail.com>
2025-09-07 02:29:46 +0600

    SYS.EXIT

--- CS50P - Harvard/CS50P - Lecture 4/SYS-Exit.py
+++ CS50P - Harvard/CS50P - Lecture 4/SYS-Exit.py
@@ -2,9 +2,9 @@ import sys
 
 # # check for errors
 if len(sys.argv) < 2:
-    print("Too few arguments")
+    sys.exit("Too few arguments")
 elif len(sys.argv) > 2:
-    print("Too many arguments")
+    sys.exit("Too many arguments")
 
 # print the name tags
 print("Hello, my name is ", sys.argv[1])
