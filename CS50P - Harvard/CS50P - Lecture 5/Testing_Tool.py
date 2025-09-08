9c2b5d731186bd35777285073ae58ec56efa2b37
693WeeB <69xabdurrahman@gmail.com>
2025-09-08 21:31:28 +0600

    using pytest to test my calculator.py program

---
+++ CS50P - Harvard/CS50P - Lecture 5/Testing_Tool.py
@@ -0,0 +1,3 @@
+# # hear we use pytest to test my calculator.py program
+
+from cals
--- CS50P - Harvard/CS50P - Lecture 5/Using_Assert.py
+++ CS50P - Harvard/CS50P - Lecture 5/Using_Assert.py
@@ -6,8 +6,34 @@ def main():
 
 
 def test_square():
-    assert square(2) == 4
-    assert square(3) == 9
+    try:
+        assert square(2) == 4
+    
+    except AssertionError:
+        assert square(3) == 9
+    
+    try:
+        assert square(3) == 9
+    
+    except AssertionError:
+        print("Test failed: square(3) should be 9")
+        try:
+            assert square(-2) == 4
+
+        except AssertionError:
+            print("Test failed: square(-2) should be 4")
+
+    try:
+        assert square(-3) == 9
+    
+    except AssertionError:
+        print("Test failed: square(-3) should be 9")
+
+    try:
+        assert square(0) == 0
+    
+    except AssertionError:
+        print("Test failed: square(0) should be 0")
 
 
 if __name__ == "__main__":
