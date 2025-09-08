19c43b198be26209afeaf1309f514f15fd8777bf
693WeeB <69xabdurrahman@gmail.com>
2025-09-09 02:43:28 +0600

    pytest

--- CS50P - Harvard/CS50P - Lecture 5/Testing_Tool.py
+++ CS50P - Harvard/CS50P - Lecture 5/Testing_Tool.py
@@ -1,3 +1,11 @@
 # # hear we use pytest to test my calculator.py program
 
-from cals
+from calculator import square
+
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
+
