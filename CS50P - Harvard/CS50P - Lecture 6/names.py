a688bdf9be092c410542d559c0372279e2ab6578
693WeeB <69xabdurrahman@gmail.com>
2025-09-10 03:54:53 +0600

    append mode

--- CS50P - Harvard/CS50P - Lecture 6/names.py
+++ CS50P - Harvard/CS50P - Lecture 6/names.py
@@ -1,5 +1,6 @@
 name = input("Enter your name: ")
 
-file = open("names.txt", "w")
+# file = open("names.txt", "w") # # w == write mode
+file = open("names.txt", "a") # a == append mode
 file.write(name)
 file.close()
