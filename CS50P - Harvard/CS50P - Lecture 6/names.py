b4e8eeaa75eeb0ae2be45bbcb46cf5e6f3e3ae38
693WeeB <69xabdurrahman@gmail.com>
2025-09-10 04:01:40 +0600

    use f strings and "\n" to separet the names in new line every time

--- CS50P - Harvard/CS50P - Lecture 6/names.py
+++ CS50P - Harvard/CS50P - Lecture 6/names.py
@@ -1,6 +1,6 @@
 name = input("Enter your name: ")
 
-# file = open("names.txt", "w") # # w == write mode
-file = open("names.txt", "a") # a == append mode
-file.write(name)
+# file = open("names.txt", "w")  # # w == write mode
+file = open("names.txt", "a")  # a == append mode
+file.write(f"{name}\n")
 file.close()
