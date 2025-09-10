a8166c2058ab5c34be0ca79a4c224450bdcbad72
693WeeB <69xabdurrahman@gmail.com>
2025-09-10 19:42:36 +0600

    # In this code I use rstrip() function to remove the new line # from the names in the text file

--- CS50P - Harvard/CS50P - Lecture 6/Rstrips.py
+++ CS50P - Harvard/CS50P - Lecture 6/Rstrips.py
@@ -1,3 +1,7 @@
+# In this code I use rstrip() function to remove the new line 
+# from the names in the text file
+
+
 with open("names.txt", "r") as file:
     for line in file:
         print("hello, ", line.rstrip())
