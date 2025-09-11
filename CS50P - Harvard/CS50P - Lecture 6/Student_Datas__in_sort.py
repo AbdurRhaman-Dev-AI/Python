eb5d5234f3673ef86ae4a8d43b5b5ea8cec3dd6d
693WeeB <69xabdurrahman@gmail.com>
2025-09-11 14:11:20 +0600

    # # in this code we are use sorted() function split name and plase top to botom upsite down

---
+++ CS50P - Harvard/CS50P - Lecture 6/Student_Datas__in_sort.py
@@ -0,0 +1,11 @@
+# # in this code we are use sorted() function split name and plase top to botom upsite down
+students = []
+
+with open("student.csv") as file:
+    for line in file:
+        name, place = line.rstrip().split(",")
+        students.append(f"{name} lives in {place}")
+
+
+for student in sorted(students):
+    print(student)
