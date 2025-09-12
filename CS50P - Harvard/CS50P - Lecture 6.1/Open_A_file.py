caf7ff556a5cb8536605c48b57daae6fe7ac8df6
693WeeB <69xabdurrahman@gmail.com>
2025-09-13 05:12:33 +0600

    # # in this code we are going to learn how to create a file by using open function and write someting on the txt file

--- CS50P - Harvard/CS50P - Lecture 6.1/Open_A_file.py
+++ CS50P - Harvard/CS50P - Lecture 6.1/Open_A_file.py
@@ -1,4 +1,4 @@
-# # in this code we are going to learn how to create a file by using open function
+# # in this code we are going to learn how to create a file by using open function and write someting on the txt file
 
 name = input("what is your name? ")
 file = open("Names.txt", "w")
