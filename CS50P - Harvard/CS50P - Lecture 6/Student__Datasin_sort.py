de04895f0cfb96d91b8a0931f57fe90993b10033
693WeeB <69xabdurrahman@gmail.com>
2025-09-12 18:36:22 +0600

    students.csv

---
+++ CS50P - Harvard/CS50P - Lecture 6/Student__Datasin_sort.py
@@ -0,0 +1,12 @@
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, place = line.strip().split(",")
+        student = {}
+        student["name"] = name
+        student["place"] = place
+        students.append(student)
+
+for student in students:
+    print(f"{student['name']} lives in {student['place']}")
