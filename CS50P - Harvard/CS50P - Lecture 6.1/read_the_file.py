f827324edaa771c7ee9e30ea0ad7bfe1ccf6f3d3
693WeeB <69xabdurrahman@gmail.com>
2025-09-13 16:14:03 +0600

    # # and i can use end="" to remove the new line character same as the strip function

--- CS50P - Harvard/CS50P - Lecture 6.1/read_the_file.py
+++ CS50P - Harvard/CS50P - Lecture 6.1/read_the_file.py
@@ -1,8 +1,10 @@
 # # in this code i open the file in read mode and use readlines function
 # # and use strip function to remove the new line character
+# # and i can use end="" to remove the new line character same as the strip function
 
 with open("Names.txt", "r") as file:
     lines = file.readlines()
 
 for line in lines:
+    # print("hello "+line, end="")
     print("hello "+line.strip())
