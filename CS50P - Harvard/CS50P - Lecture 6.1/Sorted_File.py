a266d41c2698d3a0ecc16eee00bd3179a95244fb
693WeeB <69xabdurrahman@gmail.com>
2025-09-13 21:25:02 +0600

    # # in this code we open the file using with function to open and close the file automatically # # an we use the sorted function to sort the file # # and we use strip function to remove the new line character # # and we can use end="" to remove the new line character same as the strip function

---
+++ CS50P - Harvard/CS50P - Lecture 6.1/Sorted_File.py
@@ -0,0 +1,10 @@
+# # in this code we open the file using with function to open and close the file automatically
+# # an we use the sorted function to sort the file
+# # and we use strip function to remove the new line character
+# # and we can use end="" to remove the new line character same as the strip function
+
+
+with open("Names.txt") as file:
+    for line in sorted(file):
+        # print("hello" ,line, end="")
+        print("hello", line.strip())
